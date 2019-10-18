223ae3b2ddc3c621a814b5efc89637b46645b332
Prashanthi-Admin <dornalaprashanthi@gmail.com>
2019-10-17 21:51:06 -0500

    Admin second change2

--- CS504_CMS/DataConversion.py
+++ CS504_CMS/DataConversion.py
@@ -2,5 +2,6 @@ price=10
 print("Hello")
 print('How many beers you want sir?')
 print('your total price is:  $' +str(price*int(input())))
+print("Party time")
 Print('Have a great day')
 print("See you")
