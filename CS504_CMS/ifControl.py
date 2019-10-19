419fd17c0c9d5854136552321742eabcea7f1f1b
Prashanthi-Admin <dornalaprashanthi@gmail.com>
2019-10-19 16:28:56 -0500

    bug fixes in master branch

--- CS504_CMS/ifControl.py
+++ CS504_CMS/ifControl.py
@@ -3,7 +3,7 @@ age=int(input())
 
 if age < 22:
     print('You are too young to have a drink.')
-elif age >= 80:
+elif age >= 65:
     print('Ok, you will get a free drink.')
 else:
     print('Sure, enjoy your drink.')
